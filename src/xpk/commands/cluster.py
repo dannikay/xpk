713fc4dab5d3f1a0217b45ffa659b82fa265c324
Xiaochu Liu <xiaochu@google.com>
2025-08-23 01:19:50 +0000

    Disable cluster autoscaling

--- src/xpk/commands/cluster.py
+++ src/xpk/commands/cluster.py
@@ -1101,13 +1101,13 @@ def run_gke_cluster_create_command(
       f' --node-locations={args.zone}'
       f' --cluster-version={gke_control_plane_version}'
       f' --machine-type={machine_type}'
-      ' --enable-autoscaling'
+      # ' --enable-autoscaling'
       ' --total-min-nodes 1 --total-max-nodes 1000'
       f' --num-nodes {args.default_pool_cpu_num_nodes}'
       f' {args.custom_cluster_arguments}'
       f' {rapid_release_cmd}'
       ' --enable-dns-access'
-      ' --autoscaling-profile=optimize-utilization'
+      # ' --autoscaling-profile=optimize-utilization'
   )
 
   enable_ip_alias = False
